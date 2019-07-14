75ba9db01de6fe0922ebb2330cdfee52a0579503
Honghao Zeng <nat@nat.moe>
2019-07-13 23:44:00 -0400

    bgp-log: doc

--- model/bgp-log.cc
+++ model/bgp-log.cc
@@ -1,3 +1,13 @@
+/**
+ * @file bgp-log.cc
+ * @author Nato Morichika <nat@nat.moe>
+ * @brief libbgp log forwarder.
+ * @version 0.1
+ * @date 2019-07-13
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include <string.h>
 #include "bgp-log.h"
 #include "ns3/log.h"
@@ -6,6 +16,11 @@ namespace ns3 {
 
 NS_LOG_COMPONENT_DEFINE("BgpLog");
 
+/**
+ * @brief Construct a new BgpLog object
+ * 
+ * @param owner The owner (prefix) of the log message
+ */
 BgpLog::BgpLog(const char *owner) {
     this->owner = strdup(owner);
 }
@@ -18,6 +33,11 @@ void BgpLog::logImpl(const char* str) {
     NS_LOG_INFO(owner << " libbgp: " << str);
 }
 
+/**
+ * @brief Set the owner (prefix) of the log message.
+ * 
+ * @param owner 
+ */
 void BgpLog::SetOwner(const char* owner) {
     free(this->owner);
     this->owner = strdup(owner);
--- model/bgp-log.h
+++ model/bgp-log.h
@@ -1,9 +1,23 @@
+/**
+ * @file bgp-log.h
+ * @author Nato Morichika <nat@nat.moe>
+ * @brief libbgp log forwarder.
+ * @version 0.1
+ * @date 2019-07-13
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef BGP_LOG_H
 #define BGP_LOG_H
 #include <libbgp/bgp-log-handler.h>
 
 namespace ns3 {
 
+/**
+ * @brief The libbgp log forwarder class.
+ * 
+ */
 class BgpLog : public libbgp::BgpLogHandler {
 public:
     BgpLog(const char *owner);
